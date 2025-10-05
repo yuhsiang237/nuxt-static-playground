ea326a6761f2801e3a76f5ffcb54cc80b3da26dd
yuhsiang237 <12533553+yuhsiang237@users.noreply.github.com>
2025-10-05 13:07:25 +0800

    feat: add deploy script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  ssr: false, // 禁用伺服器端渲染，僅生成靜態頁面
+  app: {
+    baseURL: '/nuxt-static-playground/', // 設定你的 GitHub 儲存庫名稱
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
